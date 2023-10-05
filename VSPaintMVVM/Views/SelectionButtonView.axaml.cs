514f4d73901d4a5b75e6d6adaccdd42c892dbb6c
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-10-05 22:42:18 +0700

    #Split Button Panels into SelectionButtonView and ToolButtonView

---
+++ VSPaintMVVM/Views/SelectionButtonView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VSPaintMVVM.Views;
+
+public partial class SelectionButtonView : UserControl
+{
+    public SelectionButtonView()
+    {
+        InitializeComponent();
+    }
+}

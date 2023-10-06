f6695cd934f31fc390befad47da7ddf3e89955a3
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-10-06 23:22:53 +0700

    #Added UserControl (ArtContainer modification view)

---
+++ VSPaintMVVM/Views/ArtContainerModView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VSPaintMVVM.Views;
+
+public partial class ArtContainerModView : UserControl
+{
+    public ArtContainerModView()
+    {
+        InitializeComponent();
+    }
+}

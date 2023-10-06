eb3c59684bbdd25bb0e6f7d875831e2fab0920a7
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-10-06 23:23:22 +0700

    #Added Pan and Zoom

--- VSPaintMVVM/Views/ArtContainerView.axaml.cs
+++ VSPaintMVVM/Views/ArtContainerView.axaml.cs
@@ -4,14 +4,51 @@ using Avalonia.Markup.Xaml;
 using SkiaSharp;
 using static System.Net.Mime.MediaTypeNames;
 using VSPaintMVVM.ViewModels;
+using Avalonia.Controls.PanAndZoom;
+using Avalonia.Input;
+using System.Diagnostics;
 
 namespace VSPaintMVVM.Views;
 
 public partial class ArtContainerView : UserControl
 {
-    
+    private readonly ZoomBorder? _zoomBorder;
     public ArtContainerView()
     {
         InitializeComponent();
+
+        _zoomBorder = this.Find<ZoomBorder>("ZoomBorder");
+        if (_zoomBorder != null)
+        {
+            _zoomBorder.KeyDown += ZoomBorder_KeyDown;
+
+            _zoomBorder.ZoomChanged += ZoomBorder_ZoomChanged;
+        }
+
+
+    }
+    private void ZoomBorder_KeyDown(object? sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.F:
+                _zoomBorder?.Fill();
+                break;
+            case Key.U:
+                _zoomBorder?.Uniform();
+                break;
+            case Key.R:
+                _zoomBorder?.ResetMatrix();
+                break;
+            case Key.T:
+                _zoomBorder?.ToggleStretchMode();
+                _zoomBorder?.AutoFit();
+                break;
+        }
+    }
+
+    private void ZoomBorder_ZoomChanged(object sender, ZoomChangedEventArgs e)
+    {
+        Debug.WriteLine($"[ZoomChanged] {e.ZoomX} {e.ZoomY} {e.OffsetX} {e.OffsetY}");
     }
 }

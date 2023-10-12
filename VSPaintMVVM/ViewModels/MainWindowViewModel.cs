203fa51e6d7e355c87e50bbddee736dd242fc46f
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-10-12 11:19:54 +0700

    #Temporarily give up

--- VSPaintMVVM/ViewModels/MainWindowViewModel.cs
+++ VSPaintMVVM/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,14 @@
-﻿using VSPaintMVVM.Views;
+﻿using SkiaSharp;
+using System.ComponentModel;
+using VSPaintMVVM.Tool;
+using VSPaintMVVM.Views;
 
 namespace VSPaintMVVM.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
+        
         public string Greeting => "Welcome to Avalonia!";
+
     }
 }

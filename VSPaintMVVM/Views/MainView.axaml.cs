b015bf9a5062e4183bb7cbec66043a8676c3417d
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-10-17 08:19:43 +0700

    #selecting/deselecting, erasing hovering object, strokecolor change on selecting

--- VSPaintMVVM/Views/MainView.axaml.cs
+++ VSPaintMVVM/Views/MainView.axaml.cs
@@ -10,6 +10,7 @@ using Avalonia.Input;
 using Avalonia.Media;
 using System;
 using Avalonia.Interactivity;
+using System.Linq;
 
 namespace VSPaintMVVM.Views;
 
@@ -18,7 +19,11 @@ public partial class MainView : UserControl
 
     private bool isDrawing = false;
     private bool isSelecting = false;
+    private bool isErasing = false;
+
     private List<ITool> shapeList = new List<ITool>();
+    private List<ShapeCustom> chosenList = new List<ShapeCustom>();
+
     private Stack<ITool> shapeUndoStack = new Stack<ITool>();
     private Stack<ITool> shapeRedoStack = new Stack<ITool>();
 
@@ -82,6 +87,16 @@ public partial class MainView : UserControl
         Color tempC = new Color(255, r, g, b);
         strokeColor.Fill = new SolidColorBrush(tempC).ToImmutable();
         currentColor = new SolidColorBrush(tempC);
+
+        if (isSelecting)
+        {
+            foreach (var shape in chosenList)
+            {
+                var element = shape as ITool;
+                element.Brush = currentColor;
+            }
+            Redraw();
+        }
     }
 
     //Brush size slider
@@ -96,24 +111,44 @@ public partial class MainView : UserControl
         if ((bool)SelB.IsChecked)
         {
             isSelecting = true;
+            isErasing = false;
         }    
-        else
+        else if ((bool)EraserB.IsChecked)
         {
             isSelecting = false;
+            isErasing = true;
         }
     }
     private void canvas_PointerPressed(object sender, PointerPressedEventArgs e)
     {
-        isDrawing = true;
-        Point pos = e.GetPosition(canvas);
+        if (!isSelecting || !isErasing)
+        {
+            isDrawing = true;
+            Point pos = e.GetPosition(canvas);
 
-        drawingShape.StartCorner(pos.X, pos.Y);
+            drawingShape.StartCorner(pos.X, pos.Y);
+        }    
         
     }
 
     private void canvas_PointerMoved(object sender, PointerEventArgs e)
     {
-        if (isDrawing)
+        if(isSelecting || isErasing)
+        {
+            Point pos = e.GetPosition(canvas);
+            Redraw();
+            foreach (var shape in Enumerable.Reverse(shapeList))
+            {
+                ShapeCustom element = shape as ShapeCustom;
+                if (element.isHovering(pos))
+                {
+                    if (!chosenList.Contains(element))
+                        canvas.Children.Add(((ShapeCustom)shape).drawChosenLine());
+                    break;
+                }
+            }
+        }    
+        else if (isDrawing)
         {
             Point pos = e.GetPosition(canvas);
 
@@ -127,21 +162,55 @@ public partial class MainView : UserControl
 
     private void canvas_PointerReleased(object sender, PointerReleasedEventArgs e)
     {
+        if(isSelecting)
+        {
+            Point pos = e.GetPosition(canvas);
+            foreach (var shape in Enumerable.Reverse(shapeList))
+            {
+                ShapeCustom element = shape as ShapeCustom;
+                if (element.isHovering(pos))
+                {
+                    if (chosenList.Contains(element))
+                        chosenList.Remove(element);
+                    else
+                        chosenList.Add(element);
+                    break;
+                }
+            }
+            Redraw();
+        }
 
-        Point pos = e.GetPosition(canvas);
-        drawingShape.EndCorner(pos.X, pos.Y);
+        else if (isErasing)
+        {
+            Point pos = e.GetPosition(canvas);
+            foreach (var shape in Enumerable.Reverse(shapeList))
+            {
+                ShapeCustom element = shape as ShapeCustom;
+                if (element.isHovering(pos))
+                {
+                    shapeList.Remove(shape);
+                }
+            }
+            Redraw();
+        }
 
-        // add to list
-        drawingShape.Brush = currentColor;
-        drawingShape.Thickness = currentThickness;
-        shapeList.Add(drawingShape);
-        shapeUndoStack.Push(drawingShape);
+        else
+        {
+            Point pos = e.GetPosition(canvas);
+            drawingShape.EndCorner(pos.X, pos.Y);
 
-        // new ready to draw shape
-        drawingShape = shapeCollection.Create(selectedShapeName);
+            // add to list
+            drawingShape.Brush = currentColor;
+            drawingShape.Thickness = currentThickness;
+            shapeList.Add(drawingShape);
+            shapeUndoStack.Push(drawingShape);
 
-        Redraw();
-        isDrawing=false;
+            // new ready to draw shape
+            drawingShape = shapeCollection.Create(selectedShapeName);
+
+            Redraw();
+            isDrawing = false;
+        }    
     }
 
     private void Redraw()
@@ -152,6 +221,10 @@ public partial class MainView : UserControl
         {
             var element = shape.Draw(shape.Brush, shape.Thickness);
             canvas.Children.Add(element);
+            if (chosenList.Contains(((ShapeCustom)shape)))
+            {
+                canvas.Children.Add(((ShapeCustom)shape).drawChosenLine());
+            }
         }
     }
 

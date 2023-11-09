87de363b0bcc5cad3ecb6caaf7ccba1b90cd0375
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-11-09 18:34:00 +0700

    #Redo and add shape to right position

--- VSPaintMVVM/Tool/ActionCustom.cs
+++ VSPaintMVVM/Tool/ActionCustom.cs
@@ -11,11 +11,31 @@ namespace VSPaintMVVM.Tool
     {
         public List<ITool> beforeShape = new List<ITool>();
         public List<ITool> afterShape = new List<ITool>();
-        public double beforeVar;
-        public double afterVar;
         public List<string> ids = new List<string>();
+        public List<int> pos = new List<int>();
+        public List<int> posA = new List<int>();
 
-        public virtual double ctrlZ(List<ITool> shapeList, int index)
+        public void addStarter(ITool before = null, int befPos = -1, string ID = "")
+        {
+            if (before != null || ID == "" || befPos < 0) { }
+            else
+            {
+                beforeShape.Add(before);
+                ids.Add(ID);
+                pos.Add(befPos);
+            }
+        }
+
+        public void addAfter(ITool after = null, int afPos = -1)
+        {
+            if (after != null || afPos < 0) { }
+            else
+            {
+                afterShape.Add(after);
+                posA.Add(afPos);
+            }
+        }
+        public virtual void ctrlZ(List<ITool> shapeList)
         {
             bool found = false;
             for (int j = 0; j < shapeList.Count; j++)
@@ -29,7 +49,7 @@ namespace VSPaintMVVM.Tool
                         if (beforeShape.Count == 0)
                         {
                             shapeList.Remove(shape);
-                            return -1;
+                            return;
                         }
 
                         int a = ids.IndexOf(i);
@@ -38,25 +58,60 @@ namespace VSPaintMVVM.Tool
                         temp.BoxStart = ((ShapeCustom)beforeShape[a]).BoxStart;
                         temp.BoxEnd = ((ShapeCustom)beforeShape[a]).BoxEnd;
                         temp.Angle = ((ShapeCustom)beforeShape[a]).Angle;
+                        shape.Thickness = (beforeShape[a]).Thickness;
+                        shape.Brush = (beforeShape[a]).Brush;
                     }
                 }
             }
-               
-
             //deleteAction
             if (found==false)
             {
                 foreach (var deletedShape in beforeShape)
                 {
-                    shapeList.Add(deletedShape);
+                    shapeList.Insert(pos[beforeShape.IndexOf(deletedShape)], deletedShape);
                 }
             }    
-            return beforeVar;
+            return;
         }
-        public virtual double ctrlY(ITool shape, string id) 
+        public virtual void ctrlY(List<ITool> shapeList) 
         {
+            bool found = false;
+            for (int j = 0; j < shapeList.Count; j++)
+            {
+                var shape = shapeList[j];
+                foreach (var i in ids)
+                {
+                    if (((ShapeCustom)shape).ID == i)
+                    {
+                        //deleteAction
+                        if (afterShape.Count == 0)
+                        {
+                            shapeList.Remove(shape);                            
+                            return;
+                        }
 
-            return afterVar;
+                        int a = ids.IndexOf(i);
+                        found = true;
+                        ShapeCustom temp = shape as ShapeCustom;
+                        temp.BoxStart = ((ShapeCustom)afterShape[a]).BoxStart;
+                        temp.BoxEnd = ((ShapeCustom)afterShape[a]).BoxEnd;
+                        temp.Angle = ((ShapeCustom)afterShape[a]).Angle;
+                        shape.Thickness = (afterShape[a]).Thickness;
+                        shape.Brush = (afterShape[a]).Brush;
+                    }
+                }
+            }
+
+
+            //drawAction
+            if (found == false)
+            {
+                foreach (var drawedShape in afterShape)
+                {
+                    shapeList.Insert(posA[afterShape.IndexOf(drawedShape)], drawedShape);
+                }
+            }
+            return;
         }
     }
 }

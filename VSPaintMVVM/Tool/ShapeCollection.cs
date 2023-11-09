7a6325ab8bf428ff137cbd0d093786e5707419d8
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-11-09 13:48:40 +0700

    #split shape collection as a new class

---
+++ VSPaintMVVM/Tool/ShapeCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSPaintMVVM.Shapes;
+
+namespace VSPaintMVVM.Tool
+{
+    public class ShapeCollection
+    {
+        public Dictionary<string, ITool> prototypes;
+        public ShapeCollection()
+        {
+            prototypes = new Dictionary<string, ITool>();
+
+            ITool rect = new RectangleCustom();
+
+            prototypes.Add(rect.Name, rect);
+        }
+
+        public ITool Create(string id)
+        {
+            if (prototypes[id] is ITool)
+            {
+                return prototypes[id].Clone();
+            }
+            return null;
+        }
+    }
+}

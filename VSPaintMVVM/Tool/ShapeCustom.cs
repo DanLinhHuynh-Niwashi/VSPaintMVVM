4c0238e78f97281aac8a356bd01a66228862a7fa
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-11-09 13:48:59 +0700

    #Changes in Copy funct

--- VSPaintMVVM/Tool/ShapeCustom.cs
+++ VSPaintMVVM/Tool/ShapeCustom.cs
@@ -177,7 +177,10 @@ namespace VSPaintMVVM.Tool
             {
                 BoxStart = boxStart,
                 BoxEnd = boxEnd,
+                Angle = angle,
                 ID = next_Possible_ID(),
+                showingApoints = this.showingApoints,
+                apoints = this.apoints,
             };
 
             return temp;

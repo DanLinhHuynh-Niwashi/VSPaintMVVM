1db3893b839e0474657f24c4d34a1d7095575bd8
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2023-11-21 21:10:32 +0700

    #Splash screen on start

--- VSPaintMVVM/App.axaml.cs
+++ VSPaintMVVM/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using System.Threading.Tasks;
 using VSPaintMVVM.ViewModels;
 using VSPaintMVVM.Views;
 
@@ -15,18 +16,56 @@ namespace VSPaintMVVM
             AvaloniaXamlLoader.Load(this);
         }
 
-        public override void OnFrameworkInitializationCompleted()
+        public override async void OnFrameworkInitializationCompleted()
         {
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 // Line below is needed to remove Avalonia data validation.
                 // Without this line you will get duplicate validations from both Avalonia and CT
                 BindingPlugins.DataValidators.RemoveAt(0);
-                desktop.MainWindow = new MainWindow
+                // Create the splash screen
+                var splashScreenVM = new SplashScreenViewModel();
+                var splashScreen = new SplashScreen
+                {
+                    DataContext = splashScreenVM
+                };
+
+                // Set as the (temporary) main window.
+                // By default, the application lifetime will shtut down the application when the main
+                // window is closed (unless ShutdownMode is set to something else). Later on we will
+                // swap out MainWindow for the "true" MainWindow before closing the splash screen.
+                // I see that this type of MainWindow swapping is used when switching themes in
+                // Avalonias ControlCatalog source, so presumably it's OK to do this.
+                desktop.MainWindow = splashScreen;
+
+                splashScreen.Show();
+
+                try
+                {
+                    await Task.Delay(4000);
+                }
+                catch (TaskCanceledException)
+                {
+                    // User cancelled somewhere along the line. We could clean up here if needed.
+                    // Then, close the splash screen and return to shut down the application.
+                    // (If we don't close the splash screen, the app will remain running since
+                    // it is the MainWindow.)
+                    splashScreen.Close();
+                    return;
+                }
+
+                // Create the main window, and swap it in for the real main window
+                var mainWin = new MainWindow
                 {
                     DataContext = new MainWindowViewModel(),
-                    
                 };
+
+
+                desktop.MainWindow = mainWin;
+
+                mainWin.Show();
+
+                splashScreen.Close();
 
 
                 
---
+++ VSPaintMVVM/Views/SplashScreen.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using VSPaintMVVM.ViewModels;
+using ReactiveUI;
+using Avalonia.ReactiveUI;
+
+namespace VSPaintMVVM.Views
+{
+    public partial class SplashScreen : Window
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}

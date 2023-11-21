bd4f7573b8a2574cad3b18904d3101ffc2ef8773
Quynh Huynh <quynh190504@gmail.com>
2023-11-21 10:55:19 +0700

    #Add new-canvas-setting & save-current-canvas dialog view.

---
+++ VSPaintMVVM/Views/NewCanvasDialog.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VSPaintMVVM.Views
+{
+    public partial class NewCanvasDialog : Window
+    {
+        public NewCanvasDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VSPaintMVVM/Views/SaveCurrentCanvasView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VSPaintMVVM.Views
+{
+    public partial class SaveCurrentCanvasView : Window
+    {
+        public SaveCurrentCanvasView()
+        {
+            InitializeComponent();
+        }
+    }
+}
